f8d05c0e9b83ce3a4a792d5aac7242f5899a13b1
lapinRagnar <lapinragnar@gmail.com>
2023-10-10 21:12:30 +0200

    2 - ajouter route et page about && contact

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,17 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+
+  console.log('about')
+
+  return (
+    <>
+      <article>
+        <h2>A propos</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatem officia dolores in obcaecati quod et porro accusamus voluptate. Cum hic accusamus dignissimos porro facere maxime adipisci unde delectus pariatur!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatem officia dolores in obcaecati quod et porro accusamus voluptate. Cum hic accusamus dignissimos porro facere maxime adipisci unde delectus pariatur!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatem officia dolores in obcaecati quod et porro accusamus voluptate. Cum hic accusamus dignissimos porro facere maxime adipisci unde delectus pariatur!</p>
+      </article>
+    </>
+  )
+})
---
+++ src/routes/contact/index.jsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+
+export default component$(() => {
+  return (
+    
+    <article>
+      <h2>Contact</h2>
+      <p>lorem</p>
+    </article>
+    
+    
+  )
+})
